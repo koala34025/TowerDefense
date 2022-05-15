b2bb88373e78e1a5ed94303955d131b1becf7111
[koala] <koala34025@gmail.com>
2022-05-15 11:17:44 +0800

    TODO 1-6

--- TowerDefense/ArmySelectScene.cpp
+++ TowerDefense/ArmySelectScene.cpp
@@ -12,6 +12,7 @@
 #include "Resources.hpp"
 #include "Slider.hpp"
 #include "ArmySelectScene.hpp"
+#include "LOG.hpp"
 
 void ArmySelectScene::Initialize() {
     // parameter initialization
@@ -22,11 +23,12 @@ void ArmySelectScene::Initialize() {
     AddNewObject(new Engine::Image("play/sand.png", 1250, 0, 336, 896));
     
     // TODO 1 (1/8): Initialize the usedSpace and totalSpace.
+    ArmySelectScene::usedSpace = 4;
+    ArmySelectScene::totalSpace = 5;
 
-    
     // TODO 1 (2/8): Add the usedSpace and totalSpace to the label.
-    AddNewObject(UISpaceUsage = new Engine::Label("Space: 0", "pirulen.ttf", 30, 1395, 150, 0, 0, 0, 255, 0.5, 0.5));
-    
+    AddNewObject(UISpaceUsage = new Engine::Label("Space: " + std::to_string(usedSpace) + "/" + std::to_string(totalSpace), "pirulen.ttf", 30, 1395, 150, 0, 0, 0, 255, 0.5, 0.5));
+
     // initialize armyAmount
     if (!fromSetting) {
         for (int i=0; i<totalArmy; i++) {
@@ -76,6 +78,7 @@ void ArmySelectScene::Terminate() {
     IScene::Terminate();
 }
 void ArmySelectScene::PlayOnClick(ButtonType type, int id, int spaceCost) {
+    Engine::LOG() << "id: " << id;
     if (type == BUTTON_ENTER) {
         PlayScene *scene = dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetScene("play"));
         scene->SetTotalArmyAmount(totalArmy);
@@ -93,10 +96,32 @@ void ArmySelectScene::PlayOnClick(ButtonType type, int id, int spaceCost) {
     else if (type == BUTTON_ADD) {
         // TODO 1 (5/8): When the add(+) button is clicked, update the usedSpace and the armyAmount of that army. Make sure that the labels shown on the screen also update.
         // Notice that the button won't take effect when the usedSpace is equal to totalSpace.
+        if (ArmySelectScene::usedSpace == ArmySelectScene::totalSpace)
+            return;
+
+        ArmySelectScene::armyAmount[id]++;
+        ArmySelectScene::UIArmyAmount[id]->Text = std::to_string(armyAmount[id]);
+        ArmySelectScene::UIArmyAmount[id]->Draw();
+        ArmySelectScene::usedSpace++;
+        ArmySelectScene::UISpaceUsage->Text = "Space: " + std::to_string(usedSpace) + "/" + std::to_string(totalSpace);
+        ArmySelectScene::UISpaceUsage->Draw();
+
+        Engine::LOG() << "armyAmount[" << id << "]: " << ArmySelectScene::armyAmount[id] << ", usedSpace: " << ArmySelectScene::usedSpace;
     }
     else if (type == BUTTON_SUB) {
         // TODO 1 (6/8): When the sub(-) button is clicked, update the usedSpace and the armyAmount of that army. Make sure that the labels shown on the screen also update.
         // Notice that the button won't take effect when the armyAmount is equal to 0.
+        if (ArmySelectScene::armyAmount[id] == 0)
+            return;
+
+        ArmySelectScene::armyAmount[id]--;
+        ArmySelectScene::UIArmyAmount[id]->Text = std::to_string(armyAmount[id]);
+        ArmySelectScene::UIArmyAmount[id]->Draw();
+        ArmySelectScene::usedSpace--;
+        ArmySelectScene::UISpaceUsage->Text = "Space: " + std::to_string(usedSpace) + "/" + std::to_string(totalSpace);
+        ArmySelectScene::UISpaceUsage->Draw();
+
+        Engine::LOG() << "armyAmount[" << id << "]: " << ArmySelectScene::armyAmount[id] << ", usedSpace: " << ArmySelectScene::usedSpace;
     }
     else if (type == BUTTON_SETTING) {
         Engine::GameEngine::GetInstance().ChangeScene("setting");
@@ -124,10 +149,18 @@ void ArmySelectScene::AddNewArmy(int id, std::string imageName, int spaceCost) {
     // TODO 1 (3/8): Create the add(+) button. You can imitate the enter button construction in the Initialize() function.
     // Suggestion of ImageButton's position setting: x(halfW / 4 + offsetW), y(oneThirdH + 25 + offsetH), w(75), h(50).
     // Suggestion of Label position settings: x(halfW / 4 + 35 + offsetW), y(oneThirdH + 50 + offsetH).
-    
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW / 4 + offsetW, oneThirdH + 25 + offsetH, 75, 50);
+    btn->SetOnClickCallback(std::bind(&ArmySelectScene::PlayOnClick, this, BUTTON_ADD, id, spaceCost));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("+", "pirulen.ttf", 30, halfW / 4 + 35 + offsetW, oneThirdH + 50 + offsetH, 0, 0, 0, 255, 0.5, 0.5));
+
     // TODO 1 (4/8): Create the sub(-) button. You can imitate the enter button construction in the Initialize() function.
     // Suggestion of ImageButton's position settings: x(halfW / 4 + offsetW + 100), y(oneThirdH + 25 + offsetH), w(75), h(50).
     // Suggestion of Label position settings: x(halfW / 4 + 135 + offsetW), y(oneThirdH + 50 + offsetH).
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW / 4 + offsetW + 100, oneThirdH + 25 + offsetH, 75, 50);
+    btn->SetOnClickCallback(std::bind(&ArmySelectScene::PlayOnClick, this, BUTTON_SUB, id, spaceCost));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("-", "pirulen.ttf", 30, halfW / 4 + 135 + offsetW, oneThirdH + 50 + offsetH, 0, 0, 0, 255, 0.5, 0.5));
 }
 
 void ArmySelectScene::OnKeyDown(int keyCode) {
