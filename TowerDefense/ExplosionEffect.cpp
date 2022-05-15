1c0be6a1e881dde1daa8e0caa864231653b9151d
[koala] <koala34025@gmail.com>
2022-05-15 14:08:03 +0800

    todo 3

--- TowerDefense/ExplosionEffect.cpp
+++ TowerDefense/ExplosionEffect.cpp
@@ -26,3 +26,22 @@ void ExplosionEffect::Update(float deltaTime) {
 	bmp = bmps[phase];
 	Sprite::Update(deltaTime);
 }
+
+PlayScene* ShootEffect::getPlayScene() {
+	return dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetActiveScene());
+}
+ShootEffect::ShootEffect(float x, float y) : Sprite("play/shoot-1.png", x, y), timeTicks(0) {
+	for (int i = 1; i <= 4; i++) {
+		bmps.push_back(Engine::Resources::GetInstance().GetBitmap("play/shoot-" + std::to_string(i) + ".png"));
+	}
+}
+void ShootEffect::Update(float deltaTime) {
+	timeTicks += deltaTime;
+	if (timeTicks >= timeSpan) {
+		getPlayScene()->EffectGroup->RemoveObject(objectIterator);
+		return;
+	}
+	int phase = floor(timeTicks / timeSpan * bmps.size());
+	bmp = bmps[phase];
+	Sprite::Update(deltaTime);
+}
--- TowerDefense/ExplosionEffect.hpp
+++ TowerDefense/ExplosionEffect.hpp
@@ -18,4 +18,15 @@ public:
 	ExplosionEffect(float x, float y);
 	void Update(float deltaTime) override;
 };
+
+class ShootEffect : public Engine::Sprite {
+protected:
+	PlayScene* getPlayScene();
+	float timeTicks;
+	std::vector<std::shared_ptr<ALLEGRO_BITMAP>> bmps;
+	float timeSpan = 0.5;
+public:
+	ShootEffect(float x, float y);
+	void Update(float deltaTime) override;
+};
 #endif // EXPLOSIONEFFECT_HPP
